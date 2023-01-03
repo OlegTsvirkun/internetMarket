019bb601dddd1512746ced8f3d5ea4d5119ff6eb
OlegTsvirkun <dr.olegtsvirkun@gmail.com>
2023-01-03 15:04:19 +0200

    refix

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import { OrderPage } from "./pages/OrderPage/OrderPage";
 import './App.css';
 import { SearchPage } from "./pages/SearchPage/SearchPage";
 import { Footer } from "./components/Footer/Footer";
+import { FinishOrder } from "./pages/FinishOrder/FinishOrder";
 
 function App() {
   // const [searchValue, setSearchValue] = useState('');
@@ -28,6 +29,7 @@ function App() {
             <Route exact path="/cat/:category" element={<CategoryPage />} />
             <Route exact path="/order" element={<OrderPage />} />
             <Route exact path="/search" element={<SearchPage />} />
+            <Route exact path="/finish-order" element={<FinishOrder/>} />
           </Routes>
           <Footer/>
       </div>
